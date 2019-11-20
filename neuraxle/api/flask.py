a96b8eaa9939dcc62f4c9af78f751680c31cc620
alexbrillant <abrillant23@gmail.com>
2019-11-20 13:12:46 -0500

    Fix JSONDataReponseEncoder for np.ndarray

--- neuraxle/api/flask.py
+++ neuraxle/api/flask.py
@@ -60,8 +60,7 @@ class JSONDataResponseEncoder(NonFittableMixin, BaseStep, ABC):
         :return: flask response object
         """
         from flask import jsonify
-        if isinstance(data_inputs, np.ndarray):
-            data_inputs = data_inputs.tolist()
+        data_inputs = np.array(data_inputs).tolist()
 
         return jsonify(self.encode(data_inputs))
 
